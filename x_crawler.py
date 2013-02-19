6299e8d0b41983933acb6b8dcc33ba2e307e9eb0
eucho <eucho@debian.(none)>
2013-02-19 22:01:04 +0800

    bs4

--- x_crawler.py
+++ x_crawler.py
@@ -1,39 +1,38 @@
 import urllib2
 import time
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import re
 
 def crawl(frm, to):
 	opener = urllib2.build_opener()
 	site = 'http://acm.timus.ru/'
-	uri = 'status.aspx?space=1&from=%s&count=100' % frm
+	uri = 'status.aspx?space=1&from=%s' % frm
 	fw = open('ural.%s_%s'%(frm, to), 'w')
 	while frm >= to:
 		print frm, to, uri
 		req = urllib2.Request(site + uri + '&count=100')
 		r = opener.open(req)
-		#r = open('300.html', 'r')
 		soup = BeautifulSoup(r.read())
 
 		#list columns in need in current page
-		list_id = soup.findAll(attrs={'class':'id'})
-		list_pid = soup.findAll(attrs={'class':'problem'})
-		list_res = soup.findAll(attrs={'class':re.compile('^verdict')})
+		list_id = soup.find_all(attrs={'class':'id'})
+		list_pid = soup.find_all(attrs={'class':'problem'})
+		list_res = soup.find_all(attrs={'class':re.compile('^verdict')})
 
 		#write into file
 		list_all = []
 		for item in zip(list_id[1:], list_pid[1:], list_res):
-			list_all.append(item[0].encodeContents('ascii') + '\t' + \
-				item[1].a.encodeContents('ascii')[:4] + '\t' + \
-				item[2].encodeContents('ascii') + '\n')
+			list_all.append(item[0].string + '\t' + \
+				item[1].a.string[:4] + '\t' + \
+				item[2].string + '\n')
 		fw.writelines(list_all)
 		fw.flush()
 
 		#find next page
-		uri = soup.findAll(text=re.compile('Next'))[-1].parent['href']
+		uri = soup.find_all(text=re.compile('Next'))[-1].parent['href']
 		frm = int(uri.split('=')[-1])
 	
 	fw.close()
 
 if __name__ == '__main__':
-	crawl(5000,4500)
+	crawl(50000,49000)
