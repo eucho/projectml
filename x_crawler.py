98bda904a25aa5539f337eb3f48752efbe9a64bd
eucho <eucho@debian.(none)>
2013-02-19 22:53:19 +0800

    add column coder

--- x_crawler.py
+++ x_crawler.py
@@ -17,14 +17,16 @@ def crawl(frm, to):
 		#list columns in need in current page
 		list_id = soup.find_all(attrs={'class':'id'})
 		list_pid = soup.find_all(attrs={'class':'problem'})
+		list_coder = [x.a for x in soup.find_all(attrs={'class':'coder'})]
 		list_res = soup.find_all(attrs={'class':re.compile('^verdict')})
 
 		#write into file
 		list_all = []
-		for item in zip(list_id[1:], list_pid[1:], list_res):
-			list_all.append(item[0].string + '\t' + \
-				item[1].a.string[:4] + '\t' + \
-				item[2].string + '\n')
+		for item in zip(list_id[1:], list_pid[1:], list_coder[1:], list_res):
+			list_all.append(item[0].string.encode('ascii') + '\t' + \
+				item[1].a.string[:4].encode('ascii') + '\t' + \
+				item[2].string.encode('ascii') + '\t' + \
+				item[3].string.encode('ascii') + '\n')
 		fw.writelines(list_all)
 		fw.flush()
 
@@ -35,4 +37,4 @@ def crawl(frm, to):
 	fw.close()
 
 if __name__ == '__main__':
-	crawl(50000,49000)
+	crawl(50000,49500)
