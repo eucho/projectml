ab664999b4c4c27795d6d28d7b0badea60372722
eucho <appotoxin@gmail.com>
2013-02-24 16:31:31 +0800

    modified list_pid due to the change of URAL

--- x_crawler.py
+++ x_crawler.py
@@ -2,6 +2,7 @@ import urllib2
 import time
 from bs4 import BeautifulSoup
 import re
+import sys
 
 def crawl(frm, to):
 	opener = urllib2.build_opener()
@@ -9,10 +10,17 @@ def crawl(frm, to):
 	uri = 'status.aspx?space=1&from=%s' % frm
 	fw = open('ural.%s_%s'%(frm, to), 'w')
 	while frm >= to:
-		print frm, to, uri
+		print frm, to
 		req = urllib2.Request(site + uri + '&count=100')
-		r = opener.open(req)
-		soup = BeautifulSoup(r.read())
+		while True:
+                        try:
+                                print uri
+                                r = opener.open(req, timeout = 3000)
+                                soup = BeautifulSoup(r.read())
+                        except:
+                                print 'Open failed!'
+                        else:
+                                break
 
 		#list columns in need in current page
 		list_id = soup.find_all(attrs={'class':'id'})
@@ -23,8 +31,8 @@ def crawl(frm, to):
 		#write into file
 		list_all = []
 		for item in zip(list_id[1:], list_pid[1:], list_coder[1:], list_res):
-			list_all.append(item[0].string + '\t' + \
-				item[1].a.string[:4] + u'\t' + \
+			list_all.append(item[0].string + u'\t' + \
+				item[1].a['href'][-4:] + u'\t' + \
 				item[2].string + u'\t' + \
 				item[3].string + u'\n')
 		fw.writelines([x.encode('utf-8') for x in list_all])
@@ -37,4 +45,10 @@ def crawl(frm, to):
 	fw.close()
 
 if __name__ == '__main__':
-	crawl(33745,30000)
+        try:
+                frm = int(sys.argv[1])
+                to = int(sys.argv[2])
+        except:
+                print 'invalid args'
+                exit(1)
+        crawl(frm, to)
